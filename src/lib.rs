34896ca009b3778555d61a149eb6ed16dd5d9448
ko1N <ko1N1337@gmail.com>
2020-04-20 21:19:24 +0200

    minor update to make code more neat

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,3 @@ pub use vector3::*;
 pub trait IsFinite {
     fn is_finite(&self) -> bool;
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
