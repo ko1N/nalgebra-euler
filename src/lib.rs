1f45330062376824b1996d8dfa66b20227c5f925
ko1N <ko1N1337@gmail.com>
2022-12-20 03:55:27 +0100

    Added forward export of nalgebra to prevent version issues

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,15 @@ pub use vector3::*;
 pub trait IsFinite {
     fn is_finite(&self) -> bool;
 }
+
+pub mod prelude {
+    pub mod v1 {
+        pub use crate::euler::*;
+        pub use crate::float::*;
+        pub use crate::vector3::*;
+        pub use crate::IsFinite;
+
+        // external libraries
+        pub use ::nalgebra;
+    }
+}
